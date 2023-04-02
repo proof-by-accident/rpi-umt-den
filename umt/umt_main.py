6f6b4921a8007d7eea8fa88af02329860ab1d1bf
proof-by-accident <petersh@ffery.net>
2023-04-01 18:06:24 -0600

    Rewriting Dockerfile for a smoother build

--- umt/umt_main.py
+++ umt/umt_main.py
@@ -1,4 +1,4 @@
-#--- IMPORT DEPENDENCIES ------------------------------------------------------+
+# --- IMPORT DEPENDENCIES ------------------------------------------------------+
 
 import os
 import time
@@ -10,95 +10,179 @@ import numpy as np
 # deep sort
 from deep_sort.tracker import Tracker
 from deep_sort import nn_matching
- 
+
 # umt utils
 from umt.umt_utils import parse_label_map
 from umt.umt_utils import initialize_detector
 from umt.umt_utils import initialize_img_source
 from umt.umt_utils import generate_detections
 
-#--- CONSTANTS ----------------------------------------------------------------+
+# --- CONSTANTS ----------------------------------------------------------------+
 
 LABEL_PATH = "models/tflite/coco_ssd_mobilenet_v1_1.0_quant_2018_06_29/labelmap.txt"
 DEFAULT_LABEL_MAP_PATH = os.path.join(os.path.dirname(__file__), LABEL_PATH)
-TRACKER_OUTPUT_TEXT_FILE = 'object_paths.csv'
+TRACKER_OUTPUT_TEXT_FILE = "object_paths.csv"
 
 # deep sort related
 MAX_COSINE_DIST = 0.4
 NN_BUDGET = None
 NMS_MAX_OVERLAP = 1.0
 
-#--- MAIN ---------------------------------------------------------------------+
+# --- MAIN ---------------------------------------------------------------------+
+
 
 def main():
     # parse arguments
-    parser = argparse.ArgumentParser(description='--- Raspbery Pi Urban Mobility Tracker ---')
-    parser.add_argument('-modelpath', dest='model_path', type=str, required=False, help='specify path of a custom detection model')
-    parser.add_argument('-labelmap', dest='label_map_path', default=DEFAULT_LABEL_MAP_PATH, type=str, required=False, help='specify the label map text file')
-    parser.add_argument('-imageseq', dest='image_path', type=str, required=False, help='specify an image sequence')
-    parser.add_argument('-video', dest='video_path', type=str, required=False, help='specify video file')
-    parser.add_argument('-camera', dest='camera', default=False, action='store_true', help='specify this when using the rpi camera as the input')
-    parser.add_argument('-threshold', dest='threshold', type=float, default=0.5, required=False, help='specify a custom inference threshold')
-    parser.add_argument('-tpu', dest='tpu', required=False, default=False, action='store_true', help='add this when using a coral usb accelerator')
-    parser.add_argument('-nframes', dest='nframes', type=int, required=False, default=10, help='specify nunber of frames to process')
-    parser.add_argument('-display', dest='live_view', required=False, default=False, action='store_true', help='add this flag to view a live display. note, that this will greatly slow down the fps rate.')
-    parser.add_argument('-save', dest='save_frames', required=False, default=False, action='store_true', help='add this flag if you want to persist the image output. note, that this will greatly slow down the fps rate.')
+    parser = argparse.ArgumentParser(
+        description="--- Raspbery Pi Urban Mobility Tracker ---"
+    )
+    parser.add_argument(
+        "-modelpath",
+        dest="model_path",
+        type=str,
+        required=False,
+        help="specify path of a custom detection model",
+    )
+    parser.add_argument(
+        "-labelmap",
+        dest="label_map_path",
+        default=DEFAULT_LABEL_MAP_PATH,
+        type=str,
+        required=False,
+        help="specify the label map text file",
+    )
+    parser.add_argument(
+        "-imageseq",
+        dest="image_path",
+        type=str,
+        required=False,
+        help="specify an image sequence",
+    )
+    parser.add_argument(
+        "-video", dest="video_path", type=str, required=False, help="specify video file"
+    )
+    parser.add_argument(
+        "-camera",
+        dest="camera",
+        default=False,
+        action="store_true",
+        help="specify this when using the rpi camera as the input",
+    )
+    parser.add_argument(
+        "-threshold",
+        dest="threshold",
+        type=float,
+        default=0.5,
+        required=False,
+        help="specify a custom inference threshold",
+    )
+    parser.add_argument(
+        "-tpu",
+        dest="tpu",
+        required=False,
+        default=False,
+        action="store_true",
+        help="add this when using a coral usb accelerator",
+    )
+    parser.add_argument(
+        "-nframes",
+        dest="nframes",
+        type=int,
+        required=False,
+        default=10,
+        help="specify nunber of frames to process",
+    )
+    parser.add_argument(
+        "-display",
+        dest="live_view",
+        required=False,
+        default=False,
+        action="store_true",
+        help="add this flag to view a live display. note, that this will greatly slow down the fps rate.",
+    )
+    parser.add_argument(
+        "-save",
+        dest="save_frames",
+        required=False,
+        default=False,
+        action="store_true",
+        help="add this flag if you want to persist the image output. note, that this will greatly slow down the fps rate.",
+    )
     args = parser.parse_args()
-    
+
     # basic checks
-    if args.model_path: assert args.label_map_path, "when specifying a custom model, you must also specify a label map path using: '-labelmap <path to labelmap.txt>'"
-    if args.model_path: assert os.path.exists(args.model_path)==True, "can't find the specified model..."
-    if args.label_map_path: assert os.path.exists(args.label_map_path)==True, "can't find the specified label map..."
-    if args.video_path: assert os.path.exists(args.video_path)==True, "can't find the specified video file..."
+    if args.model_path:
+        assert (
+            args.label_map_path
+        ), "when specifying a custom model, you must also specify a label map path using: '-labelmap <path to labelmap.txt>'"
+    if args.model_path:
+        assert (
+            os.path.exists(args.model_path) == True
+        ), "can't find the specified model..."
+    if args.label_map_path:
+        assert (
+            os.path.exists(args.label_map_path) == True
+        ), "can't find the specified label map..."
+    if args.video_path:
+        assert (
+            os.path.exists(args.video_path) == True
+        ), "can't find the specified video file..."
 
-    print('> INITIALIZING UMT...')
-    print('   > THRESHOLD:',args.threshold)
+    print("> INITIALIZING UMT...")
+    print("   > THRESHOLD:", args.threshold)
 
-	# parse label map
+    # parse label map
     labels = parse_label_map(args, DEFAULT_LABEL_MAP_PATH)
-    
+
     # initialize detector
     interpreter = initialize_detector(args)
 
     # create output directory
-    if not os.path.exists('output') and args.save_frames: os.makedirs('output')
- 
- 	# initialize deep sort tracker   
-    metric = nn_matching.NearestNeighborDistanceMetric("cosine", MAX_COSINE_DIST, NN_BUDGET)
-    tracker = Tracker(metric) 
+    if not os.path.exists("output") and args.save_frames:
+        os.makedirs("output")
+
+    # initialize deep sort tracker
+    metric = nn_matching.NearestNeighborDistanceMetric(
+        "cosine", MAX_COSINE_DIST, NN_BUDGET
+    )
+    tracker = Tracker(metric)
 
     # initialize image source
     img_generator = initialize_img_source(args)
 
     # initialize plot colors (if necessary)
-    if args.live_view or args.save_frames: COLORS = (np.random.rand(32, 3) * 255).astype(int)
+    if args.live_view or args.save_frames:
+        COLORS = (np.random.rand(32, 3) * 255).astype(int)
 
     # main tracking loop
-    print('\n> TRACKING...')
-    with open(TRACKER_OUTPUT_TEXT_FILE, 'w') as out_file:
+    print("\n> TRACKING...")
+    with open(TRACKER_OUTPUT_TEXT_FILE, "w") as out_file:
         for i, pil_img in enumerate(img_generator(args)):
-        
+
             f_time = int(time.time())
-            print('> FRAME:', i)
-            
+            print("> FRAME:", i)
+
             # add header to trajectory file
             if i == 0:
-            	header = (f'frame_num,rpi_time,obj_class,obj_id,obj_age,'
-            	    'obj_t_since_last_update,obj_hits,'
-            	    'xmin,ymin,xmax,ymax')
-            	print(header, file=out_file)
+                header = (
+                    f"frame_num,rpi_time,obj_class,obj_id,obj_age,"
+                    "obj_t_since_last_update,obj_hits,"
+                    "xmin,ymin,xmax,ymax"
+                )
+                print(header, file=out_file)
 
             # get detections
             detections = generate_detections(pil_img, interpreter, args.threshold)
-			
+
             # proceed to updating state
-            if len(detections) == 0: print('   > no detections...')
+            if len(detections) == 0:
+                print("   > no detections...")
             else:
-            
+
                 # update tracker
                 tracker.predict()
                 tracker.update(detections)
-                
+
                 # save object locations
                 if len(tracker.tracks) > 0:
                     for track in tracker.tracks:
@@ -109,47 +193,71 @@ def main():
                         row = (
                             f"{i},{f_time},{class_name},"
                             f"{track.track_id},{track.age},"
-                            f"{track.time_since_update)},{str(track.hits)},"
+                            f"{track.time_since_update},{str(track.hits)},"
                             f"{bbox[0]},{bbox[1]},"
                             f"{bbox[2]},{bbox[3]}"
                         )
->>>>>>> 81f4a6f (removing type constraints on output print; pednet sometimes returns np.nans and this breaks the output)
                         print(row, file=out_file)
-                
+
             # only for live display
             if args.live_view or args.save_frames:
-            
-            	# convert pil image to cv2
+
+                # convert pil image to cv2
                 cv2_img = cv2.cvtColor(np.array(pil_img), cv2.COLOR_RGB2BGR)
-            
-            	# cycle through actively tracked objects
+
+                # cycle through actively tracked objects
                 for track in tracker.tracks:
                     if not track.is_confirmed() or track.time_since_update > 1:
                         continue
-                    
+
                     # draw detections and label
                     bbox = track.to_tlbr()
                     class_name = labels[track.class_name]
                     color = COLORS[int(track.track_id) % len(COLORS)].tolist()
-                    cv2.rectangle(cv2_img, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])), color, 2)
-                    cv2.rectangle(cv2_img, (int(bbox[0]), int(bbox[1]-30)), (int(bbox[0])+(len(str(class_name))+len(str(track.track_id)))*17, int(bbox[1])), color, -1)
-                    cv2.putText(cv2_img, str(class_name) + "-" + str(track.track_id), (int(bbox[0]), int(bbox[1]-10)), cv2.FONT_HERSHEY_PLAIN, 1, (255,255,255), 1)
+                    cv2.rectangle(
+                        cv2_img,
+                        (int(bbox[0]), int(bbox[1])),
+                        (int(bbox[2]), int(bbox[3])),
+                        color,
+                        2,
+                    )
+                    cv2.rectangle(
+                        cv2_img,
+                        (int(bbox[0]), int(bbox[1] - 30)),
+                        (
+                            int(bbox[0])
+                            + (len(str(class_name)) + len(str(track.track_id))) * 17,
+                            int(bbox[1]),
+                        ),
+                        color,
+                        -1,
+                    )
+                    cv2.putText(
+                        cv2_img,
+                        str(class_name) + "-" + str(track.track_id),
+                        (int(bbox[0]), int(bbox[1] - 10)),
+                        cv2.FONT_HERSHEY_PLAIN,
+                        1,
+                        (255, 255, 255),
+                        1,
+                    )
 
                 # live view
                 if args.live_view:
                     cv2.imshow("tracker output", cv2_img)
                     cv2.waitKey(1)
-                    
+
                 # persist frames
-                if args.save_frames: cv2.imwrite(f'output/frame_{i}.jpg', cv2_img)
-                
-    cv2.destroyAllWindows()         
+                if args.save_frames:
+                    cv2.imwrite(f"output/frame_{i}.jpg", cv2_img)
+
+    cv2.destroyAllWindows()
     pass
 
 
-#--- MAIN ---------------------------------------------------------------------+
+# --- MAIN ---------------------------------------------------------------------+
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
-     
-#--- END ----------------------------------------------------------------------+
+
+# --- END ----------------------------------------------------------------------+
--- umt/umt_utils.py
+++ umt/umt_utils.py
@@ -11,9 +11,9 @@ import imutils
 from imutils.video import VideoStream
 
 # deep sort
-#from umt.deep_sort import generate_detections as gd
-#from umt.deep_sort.detection import Detection
-#from umt.deep_sort.preprocessing import non_max_suppression
+# from umt.deep_sort import generate_detections as gd
+# from umt.deep_sort.detection import Detection
+# from umt.deep_sort.preprocessing import non_max_suppression
 from deep_sort.detection import Detection
 from deep_sort_tools import generate_detections as gd
 
@@ -22,9 +22,9 @@ from deep_sort_tools import generate_detections as gd
 nms_max_overlap = 1.0
 
 # initialize an instance of the deep-sort tracker
-w_path = os.path.join(os.path.dirname(__file__), 'deep_sort/mars-small128.pb')
+w_path = os.path.join(os.path.dirname(__file__), "deep_sort/mars-small128.pb")
 encoder = gd.create_box_encoder(w_path, batch_size=1)
-    
+
 
 def camera_frame_gen(args):
 
@@ -50,20 +50,24 @@ def image_seq_gen(args):
     # collect images to be processed
     images = []
     for item in sorted(os.listdir(args.image_path)):
-        if item[-4:] == '.jpg': images.append(f'{args.image_path}{item}')
-    
+        if item[-4:] == ".jpg":
+            images.append(f"{args.image_path}{item}")
+
     # cycle through image sequence and yield a PIL img object
-    for frame in range(0, args.nframes): yield Image.open(images[frame])
+    for frame in range(0, args.nframes):
+        yield Image.open(images[frame])
 
 
 def video_frame_gen(args):
-    
+
     counter = 0
     cap = cv2.VideoCapture(args.video_path)
-    while(cap.isOpened()):
+    while cap.isOpened():
         counter += 1
-        if counter > args.nframes: break
-        if cv2.waitKey(1) & 0xFF == ord('q'): break
+        if counter > args.nframes:
+            break
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
 
         # pull frame from video stream
         _, frame = cap.read()
@@ -77,56 +81,61 @@ def video_frame_gen(args):
 def initialize_img_source(args):
 
     # track objects from video file
-    if args.video_path: return video_frame_gen
-    
+    if args.video_path:
+        return video_frame_gen
+
     # track objects in image sequence
-    if args.image_path: return image_seq_gen
-        
+    if args.image_path:
+        return image_seq_gen
+
     # track objects from camera source
-    if args.camera: return camera_frame_gen
+    if args.camera:
+        return camera_frame_gen
 
 
 def initialize_detector(args):
 
-    TPU_PATH = 'models/tpu/mobilenet_ssd_v2_coco_quant/mobilenet_ssd_v2_coco_quant_postprocess_edgetpu.tflite'
-    CPU_PATH = 'models/tflite/coco_ssd_mobilenet_v1_1.0_quant_2018_06_29/detect.tflite'
+    TPU_PATH = "models/tpu/mobilenet_ssd_v2_coco_quant/mobilenet_ssd_v2_coco_quant_postprocess_edgetpu.tflite"
+    CPU_PATH = "models/tflite/coco_ssd_mobilenet_v1_1.0_quant_2018_06_29/detect.tflite"
 
     # initialize coral tpu model
     if args.tpu:
-        print('   > TPU = TRUE')
-        
+        print("   > TPU = TRUE")
+
         if args.model_path:
             model_path = args.model_path
-            print('   > CUSTOM DETECTOR = TRUE')
-            print(f'      > DETECTOR PATH = {model_path}')
-        	
+            print("   > CUSTOM DETECTOR = TRUE")
+            print(f"      > DETECTOR PATH = {model_path}")
+
         else:
-        	model_path = os.path.join(os.path.dirname(__file__), TPU_PATH)
-        	print('   > CUSTOM DETECTOR = FALSE')
-        
-        _, *device = model_path.split('@')
-        edgetpu_shared_lib = 'libedgetpu.so.1'
+            model_path = os.path.join(os.path.dirname(__file__), TPU_PATH)
+            print("   > CUSTOM DETECTOR = FALSE")
+
+        _, *device = model_path.split("@")
+        edgetpu_shared_lib = "libedgetpu.so.1"
         interpreter = tflite.Interpreter(
-                model_path,
-                experimental_delegates=[
-                    tflite.load_delegate(edgetpu_shared_lib,
-                        {'device': device[0]} if device else {})
-                ])
+            model_path,
+            experimental_delegates=[
+                tflite.load_delegate(
+                    edgetpu_shared_lib, {"device": device[0]} if device else {}
+                )
+            ],
+        )
         interpreter.allocate_tensors()
 
     # initialize tflite model
     else:
-        print('   > TPU = FALSE')
-        
+        print("   > TPU = FALSE")
+
         if args.model_path:
             model_path = args.model_path
-            print('   > CUSTOM DETECTOR = TRUE')
-            print(f'      > DETECTOR PATH = {model_path}')
-        	
+            print("   > CUSTOM DETECTOR = TRUE")
+            print(f"      > DETECTOR PATH = {model_path}")
+
         else:
-        	print('   > CUSTOM DETECTOR = FALSE')
-        	model_path = os.path.join(os.path.dirname(__file__), CPU_PATH)
-        
+            print("   > CUSTOM DETECTOR = FALSE")
+            model_path = os.path.join(os.path.dirname(__file__), CPU_PATH)
+
         interpreter = tflite.Interpreter(model_path=model_path)
         interpreter.allocate_tensors()
 
@@ -134,43 +143,54 @@ def initialize_detector(args):
 
 
 def generate_detections(pil_img_obj, interpreter, threshold):
-    
+
     # resize image to match model input dimensions
-    img = pil_img_obj.resize((interpreter.get_input_details()[0]['shape'][2], 
-                              interpreter.get_input_details()[0]['shape'][1]))
+    img = pil_img_obj.resize(
+        (
+            interpreter.get_input_details()[0]["shape"][2],
+            interpreter.get_input_details()[0]["shape"][1],
+        )
+    )
 
     # add n dim
     input_data = np.expand_dims(img, axis=0)
 
     # infer image
-    interpreter.set_tensor(interpreter.get_input_details()[0]['index'], input_data)
+    interpreter.set_tensor(interpreter.get_input_details()[0]["index"], input_data)
     interpreter.invoke()
 
     # collect results
-    bboxes = np.squeeze(interpreter.get_tensor(interpreter.get_output_details()[0]['index']))
-    classes = np.squeeze(interpreter.get_tensor(interpreter.get_output_details()[1]['index']) + 1).astype(np.int32)
-    scores = np.squeeze(interpreter.get_tensor(interpreter.get_output_details()[2]['index']))
-    num = np.squeeze(interpreter.get_tensor(interpreter.get_output_details()[3]['index']))
-    
+    bboxes = np.squeeze(
+        interpreter.get_tensor(interpreter.get_output_details()[0]["index"])
+    )
+    classes = np.squeeze(
+        interpreter.get_tensor(interpreter.get_output_details()[1]["index"]) + 1
+    ).astype(np.int32)
+    scores = np.squeeze(
+        interpreter.get_tensor(interpreter.get_output_details()[2]["index"])
+    )
+    num = np.squeeze(
+        interpreter.get_tensor(interpreter.get_output_details()[3]["index"])
+    )
+
     # keep detections above specified threshold
     keep_idx = np.less(scores[np.greater(scores, threshold)], 1)
-    bboxes  = bboxes[:keep_idx.shape[0]][keep_idx]
-    classes = classes[:keep_idx.shape[0]][keep_idx]
-    scores = scores[:keep_idx.shape[0]][keep_idx]
-    
+    bboxes = bboxes[: keep_idx.shape[0]][keep_idx]
+    classes = classes[: keep_idx.shape[0]][keep_idx]
+    scores = scores[: keep_idx.shape[0]][keep_idx]
+
     # keep detections of specified classes
     #
     #
-	#...
-	
+    # ...
 
     # denormalize bounding box dimensions
     if len(keep_idx) > 0:
-        bboxes[:,0] = bboxes[:,0] * pil_img_obj.size[1]
-        bboxes[:,1] = bboxes[:,1] * pil_img_obj.size[0]
-        bboxes[:,2] = bboxes[:,2] * pil_img_obj.size[1]
-        bboxes[:,3] = bboxes[:,3] * pil_img_obj.size[0]
-    
+        bboxes[:, 0] = bboxes[:, 0] * pil_img_obj.size[1]
+        bboxes[:, 1] = bboxes[:, 1] * pil_img_obj.size[0]
+        bboxes[:, 2] = bboxes[:, 2] * pil_img_obj.size[1]
+        bboxes[:, 3] = bboxes[:, 3] * pil_img_obj.size[0]
+
     # convert bboxes from [ymin, xmin, ymax, xmax] -> [xmin, ymin, width, height]
     for box in bboxes:
         xmin = int(box[1])
@@ -178,12 +198,15 @@ def generate_detections(pil_img_obj, interpreter, threshold):
         w = int(box[3]) - xmin
         h = int(box[2]) - ymin
         box[0], box[1], box[2], box[3] = xmin, ymin, w, h
-		
+
     # generate features for deepsort
     features = encoder(np.array(pil_img_obj), bboxes)
 
     # munge into deep sort detection objects
-    detections = [Detection(bbox, score, feature, class_name) for bbox, score, feature, class_name in zip(bboxes, scores, features, classes)]
+    detections = [
+        Detection(bbox, score, feature, class_name)
+        for bbox, score, feature, class_name in zip(bboxes, scores, features, classes)
+    ]
 
     # run non-maximum suppression
     # borrowed from: https://github.com/nwojke/deep_sort/blob/master/deep_sort_app.py#L174
@@ -191,17 +214,19 @@ def generate_detections(pil_img_obj, interpreter, threshold):
     scores = np.array([d.confidence for d in detections])
     indices = non_max_suppression(boxes, nms_max_overlap, scores)
     detections = [detections[i] for i in indices]
-    
+
     return detections
 
 
 def parse_label_map(args, DEFAULT_LABEL_MAP_PATH):
-    if args.label_map_path == DEFAULT_LABEL_MAP_PATH: print('   > CUSTOM LABEL MAP = FALSE')
-    else: print(f'   > CUSTOM LABEL MAP = TRUE ({args.label_map_path})')
+    if args.label_map_path == DEFAULT_LABEL_MAP_PATH:
+        print("   > CUSTOM LABEL MAP = FALSE")
+    else:
+        print(f"   > CUSTOM LABEL MAP = TRUE ({args.label_map_path})")
 
     labels = {}
     for i, row in enumerate(open(args.label_map_path)):
-        labels[i] = row.replace('\n','')
+        labels[i] = row.replace("\n", "")
     return labels
 
 
@@ -262,7 +287,7 @@ def non_max_suppression(boxes, max_bbox_overlap, scores=None):
         overlap = (w * h) / area[idxs[:last]]
 
         idxs = np.delete(
-            idxs, np.concatenate(
-                ([last], np.where(overlap > max_bbox_overlap)[0])))
+            idxs, np.concatenate(([last], np.where(overlap > max_bbox_overlap)[0]))
+        )
 
     return pick
